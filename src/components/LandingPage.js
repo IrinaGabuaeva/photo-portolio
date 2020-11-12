40e9da6680f1ecb2a21148b0d79d8d306f56b405
Irina Gabuaeva <irina@macbook-pro.lan>
2020-11-11 19:40:39 -0500

    second commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,23 @@
+import "../App.css";
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <header className="LandingPage-header">
+        <img src="https://landingpagevideobucket.s3.us-east-2.amazonaws.com/IMG_3764+copy.mp4"></img>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        Irina Gabuaeva
+        {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
