02b22c42c8a6894fb3185a0f91a28ab369d1cab9
Irina Gabuaeva <irina@macbook-pro.lan>
2021-02-02 22:19:17 -0500

    changed folder structure for deployment on heroku

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "build", "index.html")));
+
+app.use("/api", require("./server/api"));
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Mixing it on port ${port}`));
