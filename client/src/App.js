7e2caca67d113f47a2f7fbc135cd128236d9d3cd
Irina Gabuaeva <irina@macbook-pro.lan>
2021-02-24 20:08:12 -0500

    navigation to contact part of dev page after clicking on mail icon on main page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import MainPage from "./components/photoPage/MainPage";
 import LandingPage from "./components/photoPage/LandingPage";
 import DevPage from "./components/devPage/DevPage.js";
 import Photo from "./components/photoPage/Photo";
+import Contact from "./components/devPage/Contact";
 
 function App() {
   return (
